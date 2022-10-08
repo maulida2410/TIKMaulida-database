cb9d3f5e758b8f5ae085f4b7bab8117df245423e
maulida2410 <maulidaaslamiyah@gmail.com>
2022-10-07 18:34:46 -0700

    first commit

---
+++ seputaruntirta/models.py
@@ -0,0 +1,50 @@
+from multiprocessing.managers import Namespace
+from django.db import models
+
+# Create your models here.
+
+class Kelompok(models.Model):
+    nama = models.IntegerField()
+    keterangan = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nama
+
+
+class Dosen(models.Model):
+    NIP = models.CharField(max_length=200)
+    nama = models.CharField(max_length=200)
+    tanggallahir = models.CharField(max_length=50)
+    photo = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    fakultas = models.ForeignKey(Kelompok, on_delete=models.CASCADE, null=True)
+    prodi = models.CharField(max_length=200)
+    alamat = models.TextField(max_length=200)
+
+    def __str__(self):
+        return self.nama
+
+class Staf(models.Model):
+    NIP = models.CharField(max_length=200)
+    nama = models.CharField(max_length=200)
+    tanggallahir = models.CharField(max_length=50)
+    photo = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    unit = models.CharField(max_length=200)
+    alamat = models.TextField(max_length=200)
+
+    def __str__(self):
+        return self.NIP  
+
+
+class Mahasiswa(models.Model):
+    NIM = models.CharField(max_length=200)
+    nama = models.CharField(max_length=200)
+    tanggallahir = models.CharField(max_length=50)
+    photo = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    fakultas = models.CharField(max_length=200)
+    prodi = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.NIM 
